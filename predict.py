621817f9fd930fe7c1b324d193b8ace64e07d4d8
AliBahri~ <alibahri.72.dl.k@gmail.com>
2020-02-26 17:13:08 +0330

    prediction by predic.py

---
+++ predict.py
@@ -0,0 +1,50 @@
+"""
+Created on Wed Feb 2019
+
+@author: abahri
+"""
+
+import os
+import warnings
+from skimage.io import imread, imsave
+from keras import applications
+from keras import optimizers
+from keras.utils import to_categorical
+from keras.preprocessing.image import ImageDataGenerator
+from sklearn.metrics import accuracy_score, classification_report
+import keras
+from keras import backend as K
+K.set_image_data_format('channels_last')
+from keras.models import load_model
+from glob import glob
+import numpy as np
+from keras.engine import Layer
+from keras.engine import InputSpec
+from keras.utils import conv_utils
+from model import *
+from config import *
+from loss_function import *
+
+
+def evaluate_model():
+    ## Data direction
+    data_dir= cfg["Dataset_Address_Evaluate"]
+
+    ## Data generator
+    image_data_gen_test= ImageDataGenerator(rescale=1./255)
+    image_generator_test= image_data_gen_test.flow_from_directory(data_dir, class_mode= cfg["Class_Mode"], target_size= cfg["Target_Size"], batch_size= cfg["Eval_Batch_Size"], shuffle=False)
+
+
+
+    NASNET_Model= load_model(cfg["Pretrained_Model_Path"], custom_objects={ "ICE":ICE})
+    NASNET_Model.compile(optimizer= keras.optimizers.SGD(lr= cfg["lr"], momentum= cfg["Momentum"]), loss= ICE, metrics=["accuracy"])
+    predictions= NASNET_Model.predict_generator(image_generator_test, steps=len(image_generator_test), callbacks=None, max_queue_size=10, workers=1, use_multiprocessing=False, verbose=1)
+    predictions= np.argmax(predictions, axis=-1)
+    for i in range(len(predictions)):
+        print("Image %d = %d"%(i, predictions[i]))
+
+
+if __name__ == '__main__':
+    
+    evaluate_model()
+
